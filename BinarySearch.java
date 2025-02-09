5724f444f9234c71f896a01d451b3a70adb57060
Tushar Saxena <tushar.dec6@gmail.com>
2025-02-09 18:16:29 +0530

    Created Binary Search file and write a function of it

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,32 @@
+
+import java.util.Arrays;
+
 public class BinarySearch {
-    
+    public static void main(String[] args) {
+        int arr[] = { 3, 2, 1, 4, 5 };
+        Arrays.sort(arr);
+        int target = 0;
+        int left = 0;
+        int right = arr.length - 1;
+        int result = binarySearch(arr, target, left, right);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found ");
+        }
+    }
+
+    public static int binarySearch(int arr[], int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (left + right) / 2;
+        if (target == arr[mid]) {
+            return mid;
+        } else if (target < arr[mid]) {
+            return binarySearch(arr, target, left, mid - 1);
+        } else {
+            return binarySearch(arr, target, mid + 1, right);
+        }
+    }
 }
