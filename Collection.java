a8a8e50b8f65babe14c82bcf8123c34925d08ad1
Tushar Saxena <tushar.dec6@gmail.com>
2025-02-10 16:29:11 +0530

    Created a Collection file and write functions for every collection

---
+++ Collection.java
@@ -0,0 +1,59 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Collection {
+    public List<Integer> getArrayList() {
+        List<Integer> data = new ArrayList<Integer>();
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        data.add(4);
+        data.add(3);
+        System.out.println(data.size());
+        return data;
+    }
+
+    public Set<Integer> getHashSet() {
+        Set<Integer> data2 = new HashSet<>();
+        data2.add(81);
+        data2.add(72);
+        data2.add(53);
+        data2.add(64);
+        data2.add(53);
+        System.out.println(data2.size());
+        return data2;
+    }
+
+    public Set<Integer> getTreeSet() {
+        Set<Integer> data3 = new TreeSet<Integer>();
+        data3.add(81);
+        data3.add(72);
+        data3.add(53);
+        data3.add(64);
+        data3.add(53);
+        System.out.println(data3.size());
+        return data3;
+    }
+
+    public Map<String, Integer> getHashMap() {
+        Map<String, Integer> data4 = new HashMap<>();
+        data4.put("A", 1);
+        data4.put("B", 2);
+        data4.put("C", 3);
+        data4.put("D", 4);
+        System.out.println(data4.size());
+        return data4;
+    }
+
+    public static void main(String[] args) {
+        Collection collections = new Collection();
+        Map<String,Integer> arrayList = collections.getHashMap();
+        System.out.println(arrayList);
+    }
+}
