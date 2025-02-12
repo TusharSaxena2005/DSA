f8d26a604fed84b69996bf68adcba25d47fbb682
Tushar Saxena <tushar.dec6@gmail.com>
2025-02-13 00:50:43 +0530

    Created a file Selection sort and write a function for it

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,24 @@
 public class SelectionSort {
     public static void main(String[] args) {
-        
+        int arr[] = { 64, 34, 25, 12, 22, 90, 11 };
+        for (int nums : selectionSort(arr)) {
+            System.out.print(nums + " ");
+        }
+    }
+
+    public static int[] selectionSort(int arr[]) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int maxNum = 0;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[maxNum]) {
+                    maxNum = j;
+                }
+            }
+            temp = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = arr[maxNum];
+            arr[maxNum] = temp;
+        }
+        return arr;
     }
 }
