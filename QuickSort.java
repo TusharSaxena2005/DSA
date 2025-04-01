5f02c3deab6cba76eff23c04e149ee57e81397f4
Tushar Saxena <tushar.dec6@gmail.com>
2025-04-01 17:40:48 +0530

    Circular linked list completed

--- QuickSort.java
+++ QuickSort.java
@@ -1,40 +1,40 @@
 public class QuickSort {
-    static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
+    static int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
         int i = low - 1;
 
         for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
+            if (nums[j] < pivot) {
                 i++;
-                swap(arr, i, j);
+                swap(nums, i, j);
             }
         }
 
-        swap(arr, i + 1, high);
+        swap(nums, i + 1, high);
         return i + 1;
     }
 
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
-    static void quickSort(int[] arr, int low, int high) {
+    static void quickSort(int[] nums, int low, int high) {
         if (low < high) {
 
-            int pi = partition(arr, low, high);
+            int pi = partition(nums, low, high);
 
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            quickSort(nums, low, pi - 1);
+            quickSort(nums, pi + 1, high);
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = { 2, 1, 10, 12, 3, 8, 4 };
-        int n = arr.length;
-        quickSort(arr, 0, n - 1);
-        for (int val : arr) {
+        int[] nums = { 2, 1, 10, 12, 3, 8, 4 };
+        int n = nums.length;
+        quickSort(nums, 0, n - 1);
+        for (int val : nums) {
             System.out.print(val + " ");
         }
     }
